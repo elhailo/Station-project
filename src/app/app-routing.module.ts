ae7066c31aaecd0f0127b64b3beba80d2ac51e55
elhailo <oelhail26@gmail.com>
2022-06-12 01:08:48 +0200

    add form and component indicateur

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {TheStationComponent} from "./the-station/the-station.component";
 import {StationsComponent} from "./stations/stations.component";
+import {CreateStationComponent} from "./create-station/create-station.component";
 
 const routes: Routes = [
   {path: "station-info", component: TheStationComponent,},
-  {path: "", component: StationsComponent}
+  {path: "", component: StationsComponent},
+  {path: "create-station", component: CreateStationComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,23 @@ import { StationsComponent } from './stations/stations.component';
 import { HttpClientModule } from '@angular/common/http';
 import {Stations} from "./interfaces/stations";
 import { TheStationComponent } from './the-station/the-station.component';
+import { CreateStationComponent } from './create-station/create-station.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { IndicateurStationComponent } from './indicateur-station/indicateur-station.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StationsComponent,
     TheStationComponent,
+    CreateStationComponent,
+    IndicateurStationComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
