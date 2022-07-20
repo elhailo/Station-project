18fea1dd0874ba38002c31f1d8c37a315eb8fd7c
oelhail <oelhail26@gmail.com>
2022-07-20 10:59:23 +0200

    change name project

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(compression())
 //
 // Replace the name below to match your own "defaultProject" value!
 //
-const appname = 'Station-project'
+const appname = 'station-project'
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, '..', 'dist', appname)))
